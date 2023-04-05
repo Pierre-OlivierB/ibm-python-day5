8a15fefcb513db09029390bbe1f91960417dd141
unknown <baudinopierreolivier@gmail.com>
2023-04-05 15:33:17 +0200

    explore python modul

---
+++ index.py
@@ -0,0 +1,11 @@
+# test
+# ! Modularité
+# coucou = lambda: print("Hola")
+# def coucou(): return print("Hola")
+# ttc = lambda prixHT:prixHT +(prixHT*20/100)
+def ttc(prixHT): return prixHT + (prixHT*20/100)
+
+
+print(ttc(20))
+
+# *
